df4463463bedab129cc4e1fd7ee56a41212ddf9b
sena <senika_@naver.com>
2023-02-02 01:13:07 +0900

    post
    2023-02-02-font.md

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "[Github Blog] Github 블로그 만들기",
+        "excerpt":"1. Repository 생성 깃허브 계정.github.io 라는 이름으로 레포지토리를 생성해서 로컬로 클론한다. 2. ruby, Jekyll, Bundler 설치 블로그를 참고해서 지킬을 설치했다! 이 부분은 정확히 이해 하고 있지 못해서 다시 알아볼 예정이다. https://ansohxxn.github.io/blog/i-made-my-blog/ https://jojoldu.tistory.com/288 https://codecamper.me/blog/122/ 3. 지킬 테마 선택 minimal-mistakes 테마를 선택했다. https://github.com/mmistakes/minimal-mistakes 선택한 테마의 github repository 압축 파일을 받아서 만들어둔 폴더에...","categories": ["Blog"],
+        "tags": ["Blog","Github","First Post"],
+        "url": "/blog/hello-blog/",
+        "teaser": null
+      },{
+        "title": "[Github Blog] Github 블로그 폰트 변경하기",
+        "excerpt":"font 선택하기     눈누나 Google Fonts에서 마음에 드는 폰트를 선택한다.   사용하기     main.scss 에 붙여 넣고 _variables.scss에서 선언한다.   $serif: Georgia, Times, serif !default;   사용하고 싶은 곳에서 변수처럼 사용하면 된다.  ","categories": ["Blog"],
+        "tags": ["Blog","Github","Font"],
+        "url": "/blog/font/",
+        "teaser": null
+      }]
